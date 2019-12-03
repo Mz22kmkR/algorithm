e36872703c13de862049d0ee106b412f34fde760
Mz22kmkR <51465745+Mz22kmkR@users.noreply.github.com>
2019-12-03 04:11:31 +0000

    Add files via upload

---
+++ 補完/1次補完.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	float x0,x1,f0,f1,x,h,r,f;
+	float x123,gosa;
+	printf("input x0,f0: ");
+	scanf("%f %f", &x0,&f0);
+
+	printf("input x1,f1: ");
+	scanf("%f %f",&x1,&f1);
+	printf("input x: ");
+	scanf("%f",&x);
+
+	//x123=log(x);	//演習1のやつ
+	x123=cos(x);		//演習2のやつ
+
+	h=x1-x0;
+	r=(x-x0)/h;
+	f=f0+r*(f1-f0);
+	printf("補間値 f(%f)=%f\n",x,f);
+	gosa=x123-f;
+	printf("誤差:%f\n",gosa);
+}

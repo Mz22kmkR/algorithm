14ce8a984d561008474e446ea9c862b43094be4d
mimoex <51465745+mimoex@users.noreply.github.com>
2019-12-17 02:31:51 +0000

    ルンゲクッタ法の精度がいい

---
+++ 微分方程式/オイラー法.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+float f1(float x,float y) {
+       return(-0.5*x/y);
+}
+
+int main(void) {
+   int i;
+   float x[100],y[100],h=0.2,r;
+   x[0]=0.0;
+   y[0]=2.0;
+   i=0;
+   printf("    x   ,     y     (error)\n");
+  do {
+     x[i+1]=x[i]+h;
+     y[i+1]=y[i]+f1(x[i],y[i])*h;
+     i++;
+     r=sqrt(4-x[i]*x[i]/2);
+     printf("%f, %f (%f)\n",x[i],y[i],r-y[i]);
+   } while(x[i]<2.0);
+}
---
+++ 微分方程式/ルンゲ-クッタ法.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+float f1(float x,float y) {
+       return(-0.5*x/y);
+}
+
+int main(void) {
+   int i;
+   float x[100],y[100],h=0.2,r,A,B,C,D;
+   x[0]=0.0;
+   y[0]=2.0;
+   i=0;
+   printf("    x   ,     y     (error)\n");
+   do {
+     x[i+1]=x[i]+h;
+     A=h*f1(x[i],y[i]);
+     B=h*f1(x[i]+h/2.0,y[i]+A/2.0);
+     C=h*f1(x[i]+h/2.0,y[i]+B/2.0);
+     D=h*f1(x[i]+h,y[i]+C);
+     y[i+1]=y[i]+(A+2.0*B+2.0*C+D)/6.0;
+     i++;
+     r=sqrt(4-x[i]*x[i]/2);
+     printf("%f, %f (%f)\n",x[i],y[i],r-y[i]);
+   } while(x[i]<2.0);
+} 

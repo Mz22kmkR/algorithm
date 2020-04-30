775185da57f441987285b92a0a184030aabc4cb3
mimoex <51465745+mimoex@users.noreply.github.com>
2020-04-30 10:59:12 +0900

    途中計算を表示するようにした

--- 最小二乗法/最小二乗法_1次方程式.c
+++ 最小二乗法/最小二乗法_1次方程式.c
@@ -9,21 +9,23 @@ int main(void)
 	float x[N],y[N],a,b,sx=0.0,sy=0.0,
 	sx2=0.0,sxy=0.0;
 	FILE *fp;
-	if((fp=fopen("20191119-1.dat","r"))==NULL){
+	if((fp=fopen("in.dat","r"))==NULL){
 		printf("cannot open file\n");
 		exit(0);
 	}
 	i=0;
 	while(fscanf(fp,"%f",&x[i])!=EOF&&i<N) {
-
+        
 		fscanf(fp,"%f",&y[i]);
+        printf("[%d] x:%f, y:%f\n",i,x[i],y[i]);
+
 		sx+=x[i];
 		sy+=y[i];
 		sx2+=x[i]*x[i];
 		sxy+=x[i]*y[i];
 		i++;
 	}
-
+    printf("sx:%f sy:%f sx2:%f sxy:%f\n\n",sx,sy,sx2,sxy);
 	n=i;
 	a=(sx2*sy-sx*sxy)/(n*sx2-sx*sx);
 	b=(n*sxy-sx*sy)/(n*sx2-sx*sx);
